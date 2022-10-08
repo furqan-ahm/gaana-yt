79eb165e17d384955c20ed2c5b24c4fc064a1dcc
FurqanAhmed <furqanshaheer@gmail.com>
2022-10-08 15:40:33 +0500

    created playlist model

---
+++ lib/models/playListModel.dart
@@ -0,0 +1,23 @@
+import 'package:gaana/models/songModel.dart';
+
+class PlayList{
+
+  String name;
+  List<Song> songs;
+
+  PlayList({required this.name, this.songs=const <Song>[]});
+
+
+  toMap()=>{
+    'name':name,
+    'songs':songs.map((e) => e.toMap()).toList()
+  };
+
+  static PlayList fromMap(Map data){
+    return PlayList(
+      name: data['name'],
+      songs: (data['songs'] as List<Map>).map((e) => Song.fromMap(e)).toList()
+    );
+  }
+
+}
