bbd9a3781e8433730bbc3c5edd212ce3568a03f1
FurqanAhmed <furqanshaheer@gmail.com>
2022-10-23 16:52:16 +0500

    changed downloads screen

--- lib/screens/pages/sub_views/downloads_view.dart
+++ lib/screens/pages/sub_views/downloads_view.dart
@@ -9,33 +9,30 @@ const DownloadsView({ Key? key }) : super(key: key);
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(height: 40,),
-            const Center(child: Text('Downloads', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),)),
-            const SizedBox(height: 30,),
-            Obx(
-              () {
-                final songs = controller.downloadingSongs.value; 
-                if(songs.isEmpty){
-                  return const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 20),
-                    child: Center(
-                      child: Text('Completed!'),
-                    ),
-                  );
-                }
-                return ListView.builder(
-                  shrinkWrap: true,
-                  primary: false,
-                  itemCount: songs.length,
-                  itemBuilder: (context, index)=>DownloadTile(song: songs[index]),
-                );
-              }
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: const Text('Downloads', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),),
+        centerTitle: true,
+      ),
+      body: Obx(
+        () {
+          final songs = controller.downloadingSongs.value; 
+          if(songs.isEmpty){
+            return const Padding(
+              padding: EdgeInsets.symmetric(vertical: 20),
+              child: Center(
+                child: Text('Completed!'),
+              ),
+            );
+          }
+          return ListView.builder(
+            shrinkWrap: true,
+            primary: false,
+            itemCount: songs.length,
+            itemBuilder: (context, index)=>DownloadTile(song: songs[index]),
+          );
+        }
       ),
     );
   }
